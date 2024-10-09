b4cf3815f8f1ac0ba1d2b31e3d6a19b4c35b3cd9
replyre <121796450+replyre@users.noreply.github.com>
2024-10-09 16:21:03 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setWarehouses } from "./redux/store";
 import warehouseData from "./data/warehouse.json";
